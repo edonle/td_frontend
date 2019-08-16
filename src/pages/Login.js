381c83251666fbc55e8ada28629413d9b430b6bb
Edson Carlos <edonle@gmail.com>
2019-08-16 16:42:02 -0300

    Falta CORS

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        console.log(response);
+    }
+
     return (
-        <div className="login-container">
-            <form>
+        <div className="login-container">            
+            <form onSubmit={handleSubmit}>                
                 <img src={logo} alt="Tindev" />
-                <input placeholder="Digite seu usuário no Github" />
+                <input
+                    placeholder="Digite seu usuário no Github"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit">Enviar</button>
             </form>            
         </div>                
